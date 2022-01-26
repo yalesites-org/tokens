7ef470a5e4c932b504fe55642ae6e05eccea492e
Brian Lewis <brian@fourkitchens.com>
2022-01-26 05:55:17 -0600

    feat: added yaml support to style dictionary

---
+++ sd.config.js
@@ -0,0 +1,57 @@
+const yaml = require("yaml");
+
+module.exports = {
+  parsers: [
+    {
+      // A custom parser will only run against filenames that match the pattern
+      // This pattern will match any file with the .yaml extension.
+      // This allows you to mix different types of files in your token source
+      pattern: /\.yml$/,
+      // the parse function takes a single argument, which is an object with
+      // 2 attributes: contents which is a string of the file contents, and
+      // filePath which is the path of the file.
+      // The function is expected to return a plain object.
+      parse: ({ contents, filePath }) => yaml.parse(contents),
+    },
+  ],
+  source: ["tokens/**/*.{json,jsonc,yml}"],
+  platforms: {
+    css: {
+      transforms: [
+        "attribute/cti",
+        "name/cti/kebab",
+        "time/seconds",
+        "content/icon",
+        "size/rem",
+        "color/hsl",
+      ],
+      buildPath: "build/css/",
+      files: [
+        {
+          destination: "tokens.css",
+          format: "css/variables",
+        },
+      ],
+      options: {
+        outputReferences: true,
+      },
+    },
+    json: {
+      transforms: [
+        "attribute/cti",
+        "name/cti/pascal",
+        "time/seconds",
+        "content/icon",
+        "size/rem",
+        "color/hsl",
+      ],
+      buildPath: "build/json/",
+      files: [
+        {
+          destination: "tokens.json",
+          format: "json/nested",
+        },
+      ],
+    },
+  },
+};
